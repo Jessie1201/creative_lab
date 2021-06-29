b6cf96f68ec4fc61c5983bcabc0dfbed9d163e75
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2021-06-30 00:30:44 +0200

    use dat.GUI API for real-time variable adjustment

--- three-js/first-sketch.js
+++ three-js/first-sketch.js
@@ -1,36 +1,63 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 
-let scene, camera, renderer;
+let scene, camera, renderer, cube;
 
-    // set up the environment - 
-    // initiallize scene, camera, objects and renderer
-    let init = function () {
-        // create the scene
-        scene = new THREE.Scene();
-        scene.background = new THREE.Color('black');
+let createCube = function () {
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let material = new THREE.MeshBasicMaterial({
+        color: 'blue',
+        wireframe: true
+    })
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
 
-        // create an locate the camera
-        camera = new THREE.PerspectiveCamera(30,
-            window.innerWidth / window.innerHeight,
-            1, 1000);
-        camera.position.z = 5;
+// set up the environment -
+// initiallize scene, camera, objects and renderer
+let init = function () {
+    // scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color('black');
 
-        // create the renderer   
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
+    // camera
+    camera = new THREE.PerspectiveCamera(60,
+        window.innerWidth / window.innerHeight,
+        1, 1000);
+    camera.position.z = 5;
 
-        document.body.appendChild(renderer.domElement);
-    };
+    // objects
+    // let axesHelper = new THREE.AxesHelper(5);
+    // scene.add(axesHelper);
+    createCube();
 
-    // main animation loop - calls 50-60 in a second.
-    let mainLoop = function () {
-        console.log("Hello");
-        renderer.render(scene, camera);
-        requestAnimationFrame(mainLoop);
-    };
+    // renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    ///////////////////////////////////////////////
-    init();
-    mainLoop();
+    document.body.appendChild(renderer.domElement);
+};
 
-    let geometry = new THREE.Scene
+// main animation loop - calls 50-60 in a second.
+let mainLoop = function () {
+    let ADD = 0.01;
+    console.log("Hello");
+    renderer.render(scene, camera);
+    requestAnimationFrame(mainLoop);
+    cube.rotation.x += ADD;
+    cube.rotation.y += ADD;
+    cube.rotation.z += ADD;
+};
+
+// ------------- Run program ------------ //
+init();
+mainLoop();
+
+// -------------- Debug ----------------- //
+const gui = new dat.GUI();
+gui.add(camera.position, 'x');
+gui.add(camera.position, 'y');
+gui.add(camera.position, 'z');
+
+let axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
