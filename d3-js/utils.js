92d2df68fb73742e20a0f0153a6cf38a03f6efef
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2021-07-14 23:54:08 +0200

    d3 sample radial diagram

---
+++ d3-js/utils.js
@@ -0,0 +1,9 @@
+/**
+ * get svg viewBox property
+ */
+export default function autoBox() {
+    document.body.appendChild(this);
+    const { x, y, width, height } = this.getBBox();
+    document.body.removeChild(this);
+    return [x, y, width, height];
+}
